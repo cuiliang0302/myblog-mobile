392fc786d280393cee803561fa88071cea489512
cuiliang <1554382111@qq.com>
2021-05-24 20:35:12 +0800

    修复https问题了

--- src/api/request.js
+++ src/api/request.js
@@ -4,7 +4,7 @@ export function request(config) {
   // 创建axios的实例
   const instance = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/',
-    // baseURL: 'http://myblog_api:8000/api/',
+    // baseURL: 'https://api.cuiliangblog.cn/api/',
     timeout: 10000
   })
   // 请求拦截器配置
