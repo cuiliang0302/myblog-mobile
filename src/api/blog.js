73795d44376aabdeb0fcc0c87a94d4e25825b41f
cuiliang <1554382111@qq.com>
2021-06-03 08:10:24 +0800

    添加归档页面

--- src/api/blog.js
+++ src/api/blog.js
@@ -1,16 +1,25 @@
 import index from './index'
+
 // 获取文章列表
-export function getArticle(page = 1, order = '-created_time', category) {
+export function getArticle(page = 1, order = '-created_time', category, tag) {
   if (category) {
     return index.get('blog/article/' + '?size=5&page=' + page + '&ordering=' + order + '&category=' + category)
+  }
+  if (tag) {
+    return index.get('blog/article/' + '?size=5&page=' + page + '&ordering=' + order + '&category=&tags=' + tag)
   } else {
     return index.get('blog/article/' + '?size=5&page=' + page + '&ordering=' + order)
   }
 }
 
-// 获取文章分类
+// 获取文章分类列表
 export function getCategory() {
   return index.get('blog/category/')
+}
+
+// 获取文章标签列表
+export function getTag() {
+  return index.get('blog/tag/')
 }
 
 // 获取文章详情
@@ -21,6 +30,16 @@ export function getArticleDetail(id) {
 // 获取猜你喜欢列表
 export function getGuessLike(id) {
   return index.get('blog/guessLike/' + id + '/')
+}
+
+// 获取归档全部列表
+export function getClassify() {
+  return index.get('blog/classify/')
+}
+
+// 查询指定归档文章列表
+export function getClassifyArticle(month) {
+  return index.get('blog/classify/?month=' + month)
 }
 
 // 获取笔记名称列表
--- src/views/public/Tag.vue
+++ src/views/public/Tag.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <NavBar></NavBar>
-    <Tab :tabList="tagList"></Tab>
+    <TabList :tabList="tabList" :listState="listState" @click="tabClick" @onLoad="onLoad"
+             @onRefresh="onRefresh" :load="load"></TabList>
     <Tabbar></Tabbar>
   </div>
 
@@ -10,46 +11,100 @@
 <script>
 import NavBar from "@/components/common/NavBar";
 import Tabbar from '@/components/common/Tabbar'
-import Tab from "@/components/common/Tab";
+import TabList from "@/components/common/TabList";
+import {onMounted, reactive, ref} from "vue";
+import {getArticle, gettag, getTag} from "@/api/blog";
+import {Toast} from "vant";
 
 export default {
   components: {
     NavBar,
-    Tab,
+    TabList,
     Tabbar,
   },
   name: "Tag",
   setup() {
-    // Tab 标签名
-    const tagList = [
-      {
-        title: '编程开发',
-        name: '编程开发'
-      },
-      {
-        title: '经验总结',
-        name: '经验总结'
-      },
-      {
-        title: '服务部署',
-        name: '服务部署'
-      },
-      {
-        title: '运维案例',
-        name: '运维案例'
-      },
-      {
-        title: '生活杂谈',
-        name: '生活杂谈'
-      },
-      {
-        title: '虚拟化',
-        name: '虚拟化'
+    // 加载动画
+    const load = ref(true)
+    // Tab 标签列表
+    const tabList = ref([])
+    // 文章列表数据
+    const listState = reactive({
+      list: [],
+      page: 1,
+      count: 1,
+      order: '-created_time',
+      tag: 1,
+      loading: false,
+      finished: false,
+      refreshing: false,
+    });
+    // 标签页点击切换
+    const tabClick = (index) => {
+      listState.finished = false
+      listState.list = []
+      listState.tag = index
+      listState.page = 1
+      load.value = true
+      getArticle(listState.page, listState.order, NaN, listState.tag).then((response) => {
+        console.log(response)
+        listState.page++
+        listState.list = response.results
+        listState.count = response.count
+        listState.loading = false;
+        load.value = false
+      })
+    }
+    // 子组件的加载下一页事件
+    const onLoad = () => {
+      if (listState.list.length < listState.count) {
+        getArticle(listState.page, listState.order, NaN, listState.tag).then((response) => {
+          console.log(response)
+          listState.list.push(...response.results)
+          listState.count = response.count
+          listState.loading = false;
+        })
+      } else {
+        listState.finished = true;
       }
-    ]
+    }
+    // 子组件的刷新事件
+    const onRefresh = () => {
+      listState.page = 1
+      getArticle(listState.page, listState.order, NaN, listState.tag).then((response) => {
+        console.log(response)
+        listState.list = response.results
+        listState.count = response.count
+        listState.refreshing = false;
+        Toast.success('刷新成功');
+      })
+    }
 
+    // 获取文章标签数据
+    async function tagData() {
+      tabList.value = await getTag()
+    }
+
+    // 首屏获取文章列表数据
+    async function articleData(page = 1, order = '-created_time', tag = 1) {
+      const article_data = await getArticle(page, order, NaN, tag)
+      console.log(article_data)
+      load.value = false
+      listState.list = article_data.results
+      listState.count = article_data.count
+    }
+
+    onMounted(() => {
+      tagData()
+      articleData()
+    })
     return {
-      tagList
+      tabList,
+      listState,
+      tabClick,
+      onLoad,
+      onRefresh,
+      load
     }
   }
 }
