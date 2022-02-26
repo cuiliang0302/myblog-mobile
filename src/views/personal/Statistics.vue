23ba9651e60de79aafdbbe51bd112a3b85e488c7
cuiliang <1554382111@qq.com>
2022-02-26 22:43:59 +0800

    echarts主题色

--- src/views/personal/Statistics.vue
+++ src/views/personal/Statistics.vue
@@ -103,17 +103,33 @@ import store from "@/store/index";
 let {userId, isLogin} = user()
 // 数据概览
 const dataCount = reactive({})
-// echarts曲线颜色
-const color = ref([
-  "#74b9ff",
-  "#81ecec",
-  "#55efc4",
-  "#a29bfe",
-  "#ffeaa7",
-  "#fab1a0",
-  "#ff7675",
-  "#ff7675",
-  "#b2bec3"
+// echarts明亮模式曲线颜色
+const echartsLight = ref([
+  '#c22931',
+  '#e8ad29',
+  '#008dd0',
+  '#157623',
+  '#00549d',
+  '#08919d',
+  '#009a7c',
+  '#8e44ad',
+  '#2ecc71',
+  '#f39c12',
+  '#e74c3c',
+])
+// echarts暗黑模式曲线颜色
+const echartsDark = ref([
+  '#e1605e',
+  '#3498db',
+  '#658f95',
+  '#e9937e',
+  '#7cb9a0',
+  '#f0734f',
+  '#eed875',
+  '#62996a',
+  '#5db0b3',
+  '#1abc9c',
+  '#f1c40f',
 ])
 // 是否开启暗黑模式
 const isDark = store.state.dark
@@ -149,14 +165,17 @@ async function trend() {
     section_comment.push(chartData[i].section_comment)
   }
   let myChart;
+  let color;
   if (isDark) {
     myChart = echarts.init(document.getElementById("trend"), 'dark');
+    color = echartsDark.value
   } else {
     myChart = echarts.init(document.getElementById("trend"));
+    color = echartsLight.value
   }
   // 绘制图表
   myChart.setOption({
-    color: color.value,
+    color: color,
     tooltip: {
       trigger: 'axis',
       axisPointer: {
@@ -276,14 +295,17 @@ async function time() {
     }
   })
   let myChart;
+  let color;
   if (isDark) {
     myChart = echarts.init(document.getElementById("time"), 'dark');
+    color = echartsDark.value
   } else {
     myChart = echarts.init(document.getElementById("time"));
+    color = echartsLight.value
   }
   // 绘制图表
   myChart.setOption({
-    color: color.value,
+    color: color,
     tooltip: {
       trigger: 'axis',
       axisPointer: {            // Use axis to trigger tooltip
@@ -344,14 +366,17 @@ async function article() {
   let chartData = await getEcharts(userId.value, 'article')
   console.log("article", chartData)
   let myChart;
+  let color;
   if (isDark) {
     myChart = echarts.init(document.getElementById("article"), 'dark');
+    color = echartsDark.value
   } else {
     myChart = echarts.init(document.getElementById("article"));
+    color = echartsLight.value
   }
   // 绘制图表
   myChart.setOption({
-    color: color.value,
+    color: color,
     tooltip: {
       trigger: 'item'
     },
@@ -392,14 +417,17 @@ async function note() {
     data.push(chartData[i].data)
   }
   let myChart;
+  let color;
   if (isDark) {
     myChart = echarts.init(document.getElementById("note"), 'dark');
+    color = echartsDark.value
   } else {
     myChart = echarts.init(document.getElementById("note"));
+    color = echartsLight.value
   }
   // 绘制图表
   myChart.setOption({
-    color: color.value,
+    color: color,
     legend: {
       show: false
     },
