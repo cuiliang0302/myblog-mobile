8e7e6f36775b037245f63fc9da3665943e9901c0
Liang.Cui <cuiliang0302@qq.com>
2024-11-23 13:02:21 +0800

    更新归档

--- src/views/public/Classify.vue
+++ src/views/public/Classify.vue
@@ -4,16 +4,17 @@
     <section>
       <div class="sidebar">
         <van-sidebar v-model="sidebarActive" @change="onChange">
-          <van-sidebar-item v-for="(item,index) in classifyList"
-                            :key="index"
-                            :title="formatMonth(item.month)+'('+item.count+')篇'"
+          <van-sidebar-item v-for="(data,month) in classifyList"
+                            :key="month"
+                            :title="formatMonth(month)+'('+(parseInt(data.article)+parseInt(data.section))+'篇)'"
           />
         </van-sidebar>
       </div>
       <div class="timeline">
         <van-steps direction="vertical" :active="0">
-          <van-step v-for="(item,index) in articleList" :key="index" @click="toDetail(item.id)">
-            <h3>《{{ item.title }}》</h3>
+          <van-step v-for="(item,index) in articleList" :key="index" @click="toDetail(item)">
+            <h3 v-if="item.type==='section'">笔记《{{ item.title }}》</h3>
+            <h3 v-else>文章《{{ item.title }}》</h3>
             <p>{{ timeFull(item.created_time) }}</p>
           </van-step>
         </van-steps>
@@ -29,7 +30,7 @@
 import NavBar from "@/components/common/NavBar.vue";
 import Tabbar from '@/components/common/Tabbar.vue'
 import {Step, Steps, Sidebar, SidebarItem, Toast} from 'vant';
-import {onMounted, ref} from "vue";
+import {onMounted, ref, reactive} from "vue";
 import Search from "@/views/public/Search.vue";
 import {getClassify, getClassifyArticle} from "@/api/blog";
 import timeFormat from "@/utils/timeFormat";
@@ -39,19 +40,32 @@ import {useRouter} from "vue-router";
 const router = useRouter()
 const sidebarActive = ref(0);
 //侧边月份列表
-const classifyList = ref([])
+const classifyList = reactive({})
 // 文章日期只保留天
 let {timeFull} = timeFormat()
 // 指定月份文章列表
 const articleList = ref([])
 // 侧边栏点击切换事件
 const onChange = (index) => {
-  classifyArticleData(classifyList.value[index].month)
+  console.log(index)
+  const keys = Object.keys(classifyList)
+  console.log(keys[index])
+  classifyArticleData(keys[index])
 }
 
 // 获取归档月份列表数据
-async function classifyData() {
-  classifyList.value = await getClassify()
+function classifyData() {
+  // classifyList.value = await getClassify()
+  getClassify().then((response) => {
+    console.log(response)
+    Object.keys(response).forEach((key) => {
+      classifyList[key] = response[key];
+    });
+  }).catch(response => {
+    //发生错误时执行的代码
+    console.log(response)
+    // ElMessage.error('获取归档数据失败！')
+  });
 }
 
 // 获取最近月份文章列表
@@ -64,12 +78,19 @@ const formatMonth = (value) => {
   return value.replace("-", "年") + '月'
 }
 // 跳转文章详情页
-const toDetail = (id) => {
-  router.push({path: `/detail/article/${id}`, query: {component: 'article'}})
+const toDetail = (value) => {
+  if (value['type'] === 'article') {
+    router.push('/detail/article/' + value['id'])
+  } else {
+    router.push('/detail/section/' + value['id'])
+  }
 }
 onMounted(async () => {
   await classifyData()
-  await classifyArticleData(classifyList.value[0].month)
+  const date = new Date();
+  const year = date.getFullYear(); // 获取年份
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  await classifyArticleData(year + "-" + month)
 })
 </script>
 <style lang="scss" scoped>
